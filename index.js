074e940ce71ae5e5020241e7202e073cd08db827
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-16 13:19:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  //let ride
+  if (someValue <= 400) {
+    return 'This one is on me!'
+    
+  }else  if(someValue >= 2500){
+     return 'No can do.'
+     
+  }else    {
+   return "I will gladly take your thirty bucks."
+  }
+  //return ride
 }
+scuberGreetingForFeet('199')
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  }else  {
+    return "No go."
+  }
+
+  
+  //let place = city === "Pittsburgh" ? "Ok, sounds good." : "No go."
+  
+  
 }
+ternaryCheckCity()
+ 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return "Thank you so much."
+  }else if (tip === "not as generous") {
+    return "Thank you."
+  }else {
+    return "Bye."
+  }
 }
